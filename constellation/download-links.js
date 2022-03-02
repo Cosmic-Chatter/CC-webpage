4f1088f8ebef7817fe9e076c046a0a5ded685480
Morgan Rehnberg <forceflow1049@gmail.com>
2022-03-02 11:38:13 -0600

    Created download page and logic

---
+++ constellation/download-links.js
@@ -0,0 +1,4 @@
+var downloadLinks = [
+  {os: 'mac', arch: 'mac-AS', comp: "control-server", link: "https://github.com/Cosmic-Chatter/Constellation/releases/download/v0.1-beta/control_server-0.1-beta-macOS-arm.zip"},
+  {os: 'mac', arch: 'mac-AS', comp: "media-player", link: "https://github.com/Cosmic-Chatter/Constellation/releases/download/v0.1-beta/media_player-0.1-beta-macOS-arm.app.zip"},
+]
